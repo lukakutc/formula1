3e1d63885bf09919605ffa37618c75f0dc00b9cc
Luca Tocce <luca.tocce1@gmail.com>
2025-10-16 18:14:15 -0300

    solucion

--- datos.js
+++ datos.js
@@ -7,7 +7,8 @@ const arrayEquipos = [
         historia: "Mercedes ha sido una fuerza dominante en la era híbrida desde 2014, consiguiendo múltiples campeonatos de constructores y pilotos. Cuenta con una larga tradición en la F1 y con pilotos de alto nivel cada temporada.",
         colorFondo: "radial-gradient(circle, #0d0d1a 50%, #018076 180%)",
         logoChico: "assets/img/equipos/mercedes/logo/imgi_43_2025mercedeslogowhite.webp",
-        colorCard: "#00d7b6"
+        colorCard: "#00d7b6",
+        imagenAuto: "assets/img/equipos/mercedes/auto/2025mercedescarright.webp",
     },
     {
         id:"red-bull",
@@ -17,7 +18,8 @@ const arrayEquipos = [
         historia: "Red Bull Racing ha sido uno de los equipos más exitosos en los últimos años. Con motores Honda RBPT, han conseguido varios títulos tanto de pilotos como de constructores. Max Verstappen es uno de sus pilotos más destacados.",
         colorFondo: "radial-gradient(circle, #0d0d1a 50%, #182e5fff 180%)",
         logoChico: "assets/img/equipos/redbullracing/logo/imgi_47_2025redbullracinglogowhite.webp",
-        colorCard: "#f47600"
+        colorCard: "#012f73ff",
+        imagenAuto: "assets/img/equipos/redbullracing/auto/2025redbullracingcarright.webp"
     },
     {
         id: "ferrari",
@@ -27,27 +29,19 @@ const arrayEquipos = [
         historia: "Ferrari es la escudería más antigua de las que permanecen activas en Fórmula 1, presente desde los inicios del campeonato. Símbolo de tradición, pasión y carreras épicas.",
         colorFondo: "radial-gradient(circle, #0d0d1a 50%, #710e10 180%)",
         logoChico: "assets/img/equipos/ferrari/logo/imgi_35_2025ferrarilogowhite.webp",
-        colorCard: "#ed1131"
-    },
-     {
-        id:"mclaren",
-        nombre: "McLaren Formula 1 Team",
-        logo: "assets/img/equipos/mclaren/logo/logo-mclaren.png",
-        historia: "McLaren es uno de los grandes históricos del automovilismo británico. Ha tenido épocas de gloria con múltiples campeonatos, y recientemente está intentando volver al podio con jóvenes promesas y buen rendimiento.",
-        colorFondo: "radial-gradient(circle, #0d0d1a 50%, #ff8700ff 180%)",
-        logoChico: "assets/img/equipos/mclaren/logo/imgi_41_2025mclarenlogowhite.webp",
-        nombreCorto: "McLaren",
-        colorCard: "f47600"
+        colorCard: "#ed1131",
+        imagenAuto: "assets/img/equipos/ferrari/auto/2025ferraricarright.webp"
     },
     {
         id: "aston-martin",
         nombre: "Aston Martin Aramco Formula One Team",
         nombreCorto: "Aston Martin",
-        logo: "assets/img/equipos/aston-martin/logo/logo-aston-martin.png",
+        logo: "assets/img/equipos/aston-martin/logo/pngegg.png",
         historia: "Aston Martin ha resurgido como constructor fuerte con gran respaldo financiero. En los últimos años ha mejorado su competitividad y está entre los equipos que aspiran a lo más alto.",
         colorFondo: "radial-gradient(circle, #0d0d1a 50%, #0d490dff 180%)",
         logoChico: "assets/img/equipos/aston-martin/logo/imgi_33_2025astonmartinlogowhite.webp",
-        colorCard: "#229971"
+        colorCard: "#229971",
+        imagenAuto: "assets/img/equipos/aston-martin/auto/2025astonmartincarright.webp"
     },
     {
         id: "alpine", 
@@ -57,7 +51,8 @@ const arrayEquipos = [
         historia: "Alpine, antes conocido como Renault, mantiene una sólida herencia técnica. Busca consolidarse entre los mejores con buena estrategia, desarrollo constante y talento en sus pilotos.",
         colorFondo: "radial-gradient(circle, #0d0d1a 50%, #1e3f8cff 180%)",
         logoChico: "assets/img/equipos/alpine/logo/imgi_31_2025alpinelogowhite.webp",
-        colorCard: "#00a1e8"
+        colorCard: "#00a1e8",
+        imagenAuto: "assets/img/equipos/alpine/auto/2025alpinecarright.webp"
     },
     {
         id: "haas",
@@ -67,7 +62,8 @@ const arrayEquipos = [
         historia: "Haas es el único equipo estadounidense en la parrilla fija. Aunque no ha ganado títulos, ha tenido buenos momentos y su ambición es mejorar año tras año.",
         colorFondo: "radial-gradient(circle, #0d0d1a 50%, #b6b6b6ff 180%)",
         logoChico: "assets/img/equipos/haas/logo/imgi_37_2025haaslogowhite.webp",
-        colorCard: "#9c9fa2"
+        colorCard: "#9c9fa2",
+        imagenAuto: "assets/img/equipos/haas/auto/2025haascarright.webp"
     },
      {
         id: "williams",
@@ -77,7 +73,8 @@ const arrayEquipos = [
         historia: "Williams tiene una rica historia en Fórmula 1, con múltiples campeonatos en el pasado. Aunque ha pasado por altibajos, sigue siendo una escudería emblemática y querida por los fans.",
         colorFondo: "radial-gradient(circle, #0d0d1a 50%, #5d8be0ff 180%)",
         logoChico: "assets/img/equipos/williams/logo/imgi_49_2025williamslogowhite.webp",
-        colorCard: "#1868db"
+        colorCard: "#1868db",
+        imagenAuto: "assets/img/equipos/williams/auto/2025williamscarright.webp"
     },
     {
         id: "racing-bulls",
@@ -87,9 +84,10 @@ const arrayEquipos = [
         historia: "El equipo anteriormente conocido como AlphaTauri se renombra para seguir desarrollando jóvenes talentos dentro de la estructura de Red Bull. Busca consolidar su lugar en la parrilla y ganar consistencia.",
         colorFondo: "radial-gradient(circle, #0d0d1a 50%, #425fa7ff 180%)",
         logoChico: "assets/img/equipos/racingbulls/logo/imgi_45_2025racingbullslogowhite.webp",
-        colorCard: "#6c98ff"
+        colorCard: "#6c98ff",
+        imagenAuto: "assets/img/equipos/racingbulls/auto/2025racingbullscarright.webp"
     },
-     {
+    {
         id: "kick-sauber",
         nombre: "Stake F1 Team Kick Sauber",
         nombreCorto: "Kick Sauber",
@@ -97,9 +95,21 @@ const arrayEquipos = [
         historia: "Sauber, con nombres de patrocinadores como Kick y Stake, ha sido tradicionalmente un equipo de media parrilla. Tiene buena reputación técnica y busca alianzas fuertes para subir posiciones.",
         colorFondo: "radial-gradient(circle, #0d0d1a 50%, #52E252 180%)",
         logoChico: "assets/img/equipos/kicksauber/logo/imgi_39_2025kicksauberlogowhite.webp",
-        colorCard: "#01c00e"
-    }];
-
+        colorCard: "#01c00e",
+        imagenAuto: "assets/img/equipos/kicksauber/auto/2025kicksaubercarright.webp"
+    },{
+           id:"mclaren",
+           nombre: "McLaren Formula 1 Team",
+           logo: "assets/img/equipos/mclaren/logo/logo-mclaren.png",
+           historia: "McLaren es uno de los grandes históricos del automovilismo británico. Ha tenido épocas de gloria con múltiples campeonatos, y recientemente está intentando volver al podio con jóvenes promesas y buen rendimiento.",
+           colorFondo: "radial-gradient(circle, #0d0d1a 50%, #ff8700ff 180%)",
+           logoChico: "assets/img/equipos/mclaren/logo/imgi_41_2025mclarenlogowhite.webp",
+           nombreCorto: "McLaren",
+           colorCard: "#f47600",
+           imagenAuto: "assets/img/equipos/mclaren/auto/2025mclarencarright.webp"
+       },
+    ];
+    
 
 function paginaActual(){
     
@@ -131,14 +141,14 @@ function cargarEquipos() {
                         <a href = "perfilEscuderia.html?team=${equipo.id}"><img src="${equipo.logoChico}" alt="${equipo.nombre}"></a>
                     </div>
                 </div>
-                <img src="assets/img/equipos/redbullracing/auto/2025redbullracingcarright.webp" alt="auto Red Bull"
-                    class="auto-equipo">
+                <img src="${equipo.imagenAuto}" alt="auto Red Bull" class="auto-equipo">
             </div>
         `;
         listaEquipos.appendChild(li);
         console.log(li);
     });
 };
+
 
 //cargarEquipos();
 function cargarPerfil() {
@@ -158,10 +168,6 @@ function cargarPerfil() {
             i++;
         }
     }
-    console.log("sale del bucle")
-    console.log("iterador: "+i);
-    console.log("encontrado "+encontrado);
-    console.log(arrayEquipos[i]);
     const fondo = document.querySelector(".fondo-color");
 
     if (encontrado) {
